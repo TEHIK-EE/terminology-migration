b4a36459bb13826a8bb76eca2c3c2dbcd53e6622
Igor Bossenko <igor.bossenko@gmail.com>
2023-09-27 09:42:46 +0300

    Open file with import results

--- pubkeskus/check-import.sh
+++ pubkeskus/check-import.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 python check_import.py
+
+file=$(grep 'classDateTime:' config.yml | awk '{print $2}'); 
+file="result_"$file".csv"
+echo "Last import output: $file"
+tail -f -n 1000 import-results/$file
